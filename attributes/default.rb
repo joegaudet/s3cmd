70c8311d2cb84cd5a49ff3df50c933a6185b06d9
Joe Gaudet <joe@matygo.com>
2012-11-05 13:44:44 -0800

    added a few attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,9 @@ default[:s3cmd][:users] = [:root]
 default[:s3cmd][:aws_access_key_id] = ""
 default[:s3cmd][:aws_secret_access_key] = ""
 
+node[:s3cmd][:gpg_passphrase] = ""
+node[:s3cmd][:use_https] = "True"
+
 default[:s3cmd][:version] = "v1.1.0-beta3"
 default[:s3cmd][:user] = "root"
 default[:s3cmd][:install_prefix_root] = "/usr/local"
